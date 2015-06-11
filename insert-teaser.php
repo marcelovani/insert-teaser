316e93506c59feb23186e37d8987964e4bf9e9db
Marcelo Vani <marcellovani@yahoo.co.uk>
2015-06-11 19:54:32 +0100

    Added wrappers and styling

--- insert-teaser.php
+++ insert-teaser.php
@@ -24,6 +24,9 @@ add_action( 'wp_enqueue_scripts', 'teaser_shortcode_css' );
  * @return string
  */
 function teaser_shortcode( $atts ) {
+  $post_id = 0;
+  $width = 0;
+  $height = 0;
 
   extract( shortcode_atts( array(
     'post_id'      => '',
@@ -34,25 +37,35 @@ function teaser_shortcode( $atts ) {
 
   $queried_post = get_post($post_id);
 
+  static $count;
+  if (!isset($count)) {
+    $count = 0;
+  }
+
   ob_start();
 
   if (!empty($queried_post) && $queried_post->post_status == 'publish') : ?>
 
-    <div id="teaser-<?php print $post_id; ?>" style="padding: 0;" class="teaser columns<?php echo $columns ?>">
-      <?php if (has_post_thumbnail($post_id)) : ?>
-        <div class="thumbnail">
-          <a href="<?php print $queried_post->post_name; ?>">
-            <?php print get_the_post_thumbnail($post_id, array($width, $height)); ?>
-          </a>
-        </div>
-      <?php endif; ?>
+    <div id="teaser-<?php print $post_id; ?>" class="teaser columns<?php echo $columns ?> <?php print $count % 2 == 0 ? 'even' : 'odd'; $count++; ?>">
+      <div class="left">
+        <?php if (has_post_thumbnail($post_id)) : ?>
+          <div class="thumbnail">
+            <a href="<?php print $queried_post->post_name; ?>">
+              <?php print get_the_post_thumbnail($post_id, array($width, $height)); ?>
+            </a>
+          </div>
+        <?php endif; ?>
+      </div>
 
-      <h2 class="title"><a href="<?php print $queried_post->post_name; ?>"><?php echo $queried_post->post_title; ?></a></h2>
+      <div class="right">
+        <h2 class="title"><a href="<?php print $queried_post->post_name; ?>"><?php echo $queried_post->post_title; ?></a></h2>
 
-      <?php preg_match("/(.*)<!--more-->/", $queried_post->post_content, $matches); ?>
-      <?php if (isset($matches[1])) : ?>
-        <div class="summary"><?php print $matches[1]; ?></div>
-      <?php endif; ?>
+        <?php preg_match("/(.*)<!--more-->/", $queried_post->post_content, $matches); ?>
+        <?php if (isset($matches[1])) : ?>
+          <div class="summary"><?php print $matches[1]; ?></div>
+        <?php endif; ?>
+      </div>
+
     </div>
   <?php endif;
 
